1b1307117f661d51d55f6a53ab4435d8bfcdf368
3Butterfly0 <111911754+3Butterfly0@users.noreply.github.com>
2026-04-07 22:02:36 +0530

    initial backend setup

---
+++ src/main/java/com/qubehealth/ticketmanager/TicketmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.qubehealth.ticketmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmanagerApplication.class, args);
+	}
+
+}
